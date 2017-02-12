f27065b681389a58cbb964dc886dd493a43b140a
pete gamache <pete@gamache.org>
2017-02-12 15:24:19 -0500

    filez

---
+++ .synclient-config.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+synclient VertEdgeScroll=1
+synclient HorizEdgeScroll=0
+synclient CornerCoasting=1
+synclient VertTwoFingerScroll=1
+synclient HorizTwoFingerScroll=1
+synclient TapAndDragGesture=0
+
